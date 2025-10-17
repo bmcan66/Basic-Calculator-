e357b249bb588787f7fb8307c930839215d77e34
bmcan66 <16008125037@ogr.bozok.edu.tr>
2025-10-18 00:21:41 +0300

    Update carpma-bolme.cpp

--- carpma-bolme.cpp
+++ carpma-bolme.cpp
@@ -2,15 +2,18 @@
 using namespace std;
 
 int carpmaBolme(double a, double b, char islem) {
-    if (islem == '*')
+    if (islem == '*') {
         cout << "Sonuc: " << a * b << endl;
-    else if (islem == '/')
+    }
+    else if (islem == '/') {
         if (b != 0)
             cout << "Sonuc: " << a / b << endl;
         else
             cout << "Sifira bolme hatasi!" << endl;
-    else
+    }
+    else {
         cout << "Gecersiz islem!" << endl;
+    }
 
-    return 0;
+    return 0;
 }
